f3e98d1fbde65137bec69d68b4f681c4fb171c11
SuperWanzi <super_jp@foxmail.com>
2018-03-29 21:35:47 +0800

    180329

--- NetDragonClass-VRClass/js/main.js
+++ NetDragonClass-VRClass/js/main.js
@@ -1,4 +1,5 @@
-
+showSelect();
+printAuthor();
 //点击新建作品按钮显示模态框
 $(document).on("click","#create-works",function(){
 	$("#background-fex").css("z-index","0");
@@ -7,7 +8,18 @@ $(document).on("click","#create-works",function(){
 
 $(document).on("click",".closeModal",function(){
 //	console.log($(this).parents(".modal-submit"));
-	hideModal($(this).parents(".modal-submit"))
+	var obj=$(this).parents(".modal-submit");
+	hideModal(obj);
+//	console.log(obj.attr("id"));
+	if(obj.attr("id")=="modal-author"){
+		$("#background-fex").css("z-index","0");
+	}else{
+		//设置网页能滚动
+		$("body").css("overflow","auto");
+		//关闭遮罩
+		$("#background-fex").css("display","none");
+	}
+	
 })
 $(document).on("click","#ip-image",function(){
 	$("#ipf-image").click();
@@ -32,4 +44,50 @@ $(document).on("change","#ipf-image",function(){
 		alert("上传的不是支持的图片格式");
 		$("#ip-image").val("");
 	}
+})
+
+$(document).on("click","#add-author",function(){
+	//调整遮罩层位置
+	$("#background-fex").css("z-index","2");
+	showModal($("#modal-author"));
+})
+
+$(document).on("click","#btn-add",function(){
+	var index=-1;
+	for(var i in authorData){
+		if($("#dataAuthor").val().indexOf(authorData[i])>=0){
+			index=i;
+		}
+	}
+	if(index>=0){
+		author.push(authorData[index]);
+		authorData.splice(index,1);
+		showSelect();
+	}
+	else{
+		alert("没有选择或者为空");
+	}
+	
+})
+
+$(document).on("click","#btn-remove",function(){
+	var index=-1;
+	for(var i in author){
+		if($("#selectAuthor").val().indexOf(author[i])>=0){
+			index=i;
+		}
+	}
+	if(index>=0){
+		authorData.push(author[index]);
+		author.splice(index,1);
+		showSelect();
+	}else{
+		alert("没有选择或者为空");
+	}
+	
+})
+
+$(document).on("click","#add-confirm",function(){
+	printAuthor();
+	$(this).parents(".modal-submit").find(".closeModal").click();
 })
--- NetDragonClass-VRClass/js/modal.js
+++ NetDragonClass-VRClass/js/modal.js
@@ -11,8 +11,47 @@ function showModal(obj){
 function hideModal(obj){
 	//关闭模态框
 	obj.css("display","none");
-	//设置网页能滚动
-	$("body").css("overflow","auto");
-	//关闭遮罩
-	$("#background-fex").css("display","none");
+}
+
+
+var authorData=["李明","张三丰","吴老二","李四","周杰伦","斯琴","王小二"];
+var author=[];
+
+function showSelect(){
+	$("#dataAuthor").html("");$("#selectAuthor").html("");
+	var dataAuthor=document.getElementById("dataAuthor");
+	var selectAuthor=document.getElementById("selectAuthor");
+//	console.log("do it1")
+	for(var i in authorData){
+		var option=document.createElement('option');
+		option.value=authorData[i];
+		option.innerHTML=authorData[i];
+		dataAuthor.appendChild(option);
+//		console.log("do it2")
+	}
+	for(var i in author){
+		var option=document.createElement('option');
+		option.value=author[i];
+		option.innerHTML=author[i];
+		selectAuthor.appendChild(option);
+	}
+}
+
+function printAuthor(){
+	$(".input-author").html();
+	var authorlist=document.getElementsByClassName("input-author")[0];
+	for(var i in author){
+		var authorItem=document.createElement("div");
+		authorItem.className="author-item";
+		
+		var spanname=document.createElement("span");
+		spanname.className="author-name";
+		spanname.innerHTML=author[i];
+		authorItem.appendChild(spanname);
+		var spanico=document.createElement("span");
+		spanico.className="fa fa-times";
+		authorItem.appendChild(spanico);
+		
+		authorlist.appendChild(authorItem);
+	}
 }
